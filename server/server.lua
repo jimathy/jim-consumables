1036f1dc21355e6a6e9c2ac579787316b59b5066
Jim Shield <gothicjim@gmail.com>
2022-08-15 10:06:30 +0100

    Trying to get better overriding of item control

--- server/server.lua
+++ server/server.lua
@@ -1,13 +1,9 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
---Consumables
-for k in pairs(Config.Consumables) do
-	QBCore.Functions.CreateUseableItem(k, function(source, item)
-		TriggerClientEvent('jim-consumables:Consume', source, item.name)
-	end)
-end
-for k, v in pairs(Config.Consumables) do
-	QBCore.Functions.CreateUseableItem(k, function(source, item) TriggerClientEvent('jim-consumables:Consume', source, item.name) end)
-	if not QBCore.Shared.Items[k] then print("Item check - '"..k.."' not found in the shared lua") end
-	if not Config.Emotes[v.emote] then print("Emote check - '"..k.."' requested emote '"..v.emote.."' - not found in config.lua") end
-end
+CreateThread(function()
+	for k, v in pairs(Config.Consumables) do
+		QBCore.Functions.CreateUseableItem(k, function(source, item) TriggerClientEvent('jim-consumables:Consume', source, item.name) end)
+		if not QBCore.Shared.Items[k] then print("Item check - '"..k.."' not found in the shared lua") end
+		if not Config.Emotes[v.emote] then print("Emote check - '"..k.."' requested emote '"..v.emote.."' - not found in config.lua") end
+	end
+end)
